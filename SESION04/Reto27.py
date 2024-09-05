19926cb5746bbb06bef9f103dd191debab24028f
lauracontreras25 <159088963+lauracontreras25@users.noreply.github.com>
2024-09-05 15:58:24 -0500

    Update Reto27.py

--- SESION04/Reto27.py
+++ SESION04/Reto27.py
@@ -1,9 +1,25 @@
 Reto #27: Cuenta atras 
-/*
- * Crea una función que reciba dos parámetros para crear una cuenta atrás.
- * - El primero, representa el número en el que comienza la cuenta.
- * - El segundo, los segundos que tienen que transcurrir entre cada cuenta.
- * - Sólo se aceptan números enteros positivos.
- * - El programa finaliza al llegar a cero.
- * - Debes imprimir cada número de la cuenta atrás.
- */
+
+  Crea una función que reciba dos parámetros para crear una cuenta atrás.
+- El primero, representa el número en el que comienza la cuenta.
+- El segundo, los segundos que tienen que transcurrir entre cada cuenta.
+- Sólo se aceptan números enteros positivos.
+- El programa finaliza al llegar a cero.
+- Debes imprimir cada número de la cuenta atrás.
+
+Codigo:
+import time
+
+def cuenta_atras(inicio, intervalo):
+    if inicio > 0 and intervalo > 0:
+        while inicio >= 0:
+            print(inicio)
+            time.sleep(intervalo)
+            inicio -= 1
+    else:
+        print("Los números deben ser positivos.")
+
+inicio = int(input("Ingresa el número de inicio de la cuenta atrás: "))
+intervalo = int(input("Ingresa el intervalo (en segundos) entre cada número: "))
+
+cuenta_atras(inicio, intervalo)
